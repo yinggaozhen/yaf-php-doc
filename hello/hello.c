8a46f5ab2f0b8d58a2dc4c929911ca55e2e6a4cd
gaozhen.ying <gaozhen.ying@husor.com>
2017-08-30 17:39:33 +0800

    调整代码结构

--- hello/hello.c
+++ hello/hello.c
@@ -8,58 +8,12 @@
 #include "ext/standard/info.h"
 #include "php_hello.h"
 
-extern void hello_shuffle();
-
-/* {{{ void hello_test1()
- */
-PHP_FUNCTION(hello_test1)
-{
-	ZEND_PARSE_PARAMETERS_NONE();
-
-	php_printf("The extension %s is loaded and working!\r\n", "hello");
-}
-/* }}} */
-
-/* {{{ string hello_test2( [ string $var ] )
- */
-PHP_FUNCTION(hello_test2)
-{
-	char *var = "World";
-	size_t var_len = sizeof("World") - 1;
-	zend_string *retval;
-
-	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING(var, var_len)
-	ZEND_PARSE_PARAMETERS_END();
-
-	retval = strpprintf(0, "Hello %s", var);
-
-	RETURN_STR(retval);
-}
-/* }}}*/
-
-/* {{{ PHP_RINIT_FUNCTION
- */
-PHP_RINIT_FUNCTION(hello)
-{
-#if defined(ZTS) && defined(COMPILE_DL_HELLO)
-	ZEND_TSRMLS_CACHE_UPDATE();
-#endif
-
-	return SUCCESS;
-}
-/* }}} */
-
-/* {{{ PHP_MINFO_FUNCTION
- */
 PHP_MINFO_FUNCTION(hello)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "hello support", "enabled");
 	php_info_print_table_end();
 }
-/* }}} */
 
 // hello_array_init(1, 2, 3, 4, 5);
 PHP_FUNCTION(hello_array_init)
@@ -97,19 +51,10 @@ PHP_FUNCTION(hello_array_init)
                     ZEND_HASH_FILL_ADD(entry);
                 } ZEND_HASH_FOREACH_END();
     } ZEND_HASH_FILL_END();
-
-//	RETURN_TRUE;
 }
 
 /* {{{ arginfo
  */
-ZEND_BEGIN_ARG_INFO(arginfo_hello_test1, 0)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO(arginfo_hello_test2, 0)
-	ZEND_ARG_INFO(0, str)
-ZEND_END_ARG_INFO()
-
 ZEND_BEGIN_ARG_INFO(arginfo_hello_array_init, 0)
 ZEND_END_ARG_INFO()
 /* }}} */
@@ -117,8 +62,6 @@ ZEND_END_ARG_INFO()
 /* {{{ hello_functions[]
  */
 const zend_function_entry hello_functions[] = {
-	PHP_FE(hello_test1,		arginfo_hello_test1)
-	PHP_FE(hello_test2,		arginfo_hello_test2)
 	PHP_FE(hello_array_init,		arginfo_hello_array_init)
 	PHP_FE_END
 };
@@ -130,12 +73,12 @@ zend_module_entry hello_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"hello",					/* Extension name */
 	hello_functions,			/* zend_function_entry */
-	NULL,							/* PHP_MINIT - Module initialization */
-	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
-	PHP_RINIT(hello),			/* PHP_RINIT - Request initialization */
-	NULL,							/* PHP_RSHUTDOWN - Request shutdown */
+	NULL,						/* PHP_MINIT - Module initialization */
+	NULL,						/* PHP_MSHUTDOWN - Module shutdown */
+	NULL,						/* PHP_RINIT - Request initialization */
+	NULL,						/* PHP_RSHUTDOWN - Request shutdown */
 	PHP_MINFO(hello),			/* PHP_MINFO - Module info */
-	PHP_HELLO_VERSION,		/* Version */
+	PHP_HELLO_VERSION,		    /* Version */
 	STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
