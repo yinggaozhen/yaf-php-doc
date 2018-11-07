c8a7a3efbb4c8b5b3ee3ece7ca4a4589d5336d42
Gaozhen Ying <yinggaozhen@hotmail.com>
2018-11-07 14:26:25 +0800

    add session

---
+++ Session.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Yaf;
+
+use ArrayAccess;
+use Countable;
+use Iterator;
+
+final class Session implements Iterator, ArrayAccess, Countable
+{
+    protected static $_instance = null;
+
+    /**
+     * @var array
+     */
+    protected $_session = null;
+
+    /**
+     * @var bool
+     */
+    protected $_started = false;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        $instance = self::$_instance;
+
+        if (is_null($instance)) {
+            $instance = new Session();
+            $instance->start();
+
+            if ($_SESSION == null || !is_array($_SESSION)) {
+                trigger_error("Attempt to start session failed", E_WARNING);
+                return null;
+            }
+
+            self::$_instance = $instance;
+        }
+
+        return $instance;
+    }
+
+    /**
+     * @return $this
+     */
+    public function start()
+    {
+        if ($this->_started) {
+            return $this;
+        }
+
+        session_start();
+        $this->_started = 1;
+
+        // 这里赋值其实是在instance
+        $this->_session = &$_SESSION;
+
+        return $this;
+    }
+
+    /**
+     * @param string|null $name
+     * @return array|mixed|null
+     */
+    public function get(string $name = null)
+    {
+        $session = $this->_session;
+
+        if (empty($name)) {
+            return $session;
+        }
+
+        return $session[$name] ?? null;
+    }
+
+    /**
+     * @param string $name
+     * @param $value
+     * @return $this
+     */
+    public function set(string $name, $value)
+    {
+        $this->_session[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function del($name)
+    {
+        unset($this->_session[$name]);
+
+        return $this;
+    }
+
+    public function clear(): void
+    {
+        $this->_session = [];
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function has(string $name): bool
+    {
+        return array_key_exists($name, $this->_session);
+    }
+
+    public function current()
+    {
+        return current($this->_session);
+    }
+
+    public function next()
+    {
+        return next($this->_session);
+    }
+
+    /**
+     * @return int|string
+     */
+    public function key()
+    {
+        return key($this->_session);
+    }
+
+    public function valid(): bool
+    {
+        $key = key($this->_session);
+
+       return isset($key);
+    }
+
+    public function rewind(): void
+    {
+        reset($this->_session);
+    }
+
+    public function offsetExists($offset)
+    {
+        // do nothing...
+    }
+
+    public function offsetGet($offset)
+    {
+        // do nothing...
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        // do nothing...
+    }
+
+    public function offsetUnset($offset)
+    {
+        // do nothing...
+    }
+
+    public function count(): int
+    {
+        if (empty($this->_session)) {
+            return 0;
+        }
+
+        return count($this->_session);
+    }
+
+    private function __sleep()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+}
