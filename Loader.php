27003e203d0c1d4af638b8878f61665c26fee659
Gaozhen Ying <yinggaozhen@hotmail.com>
2018-11-11 00:36:43 +0800

    从这里开始 /data/github/yaf-php/Loader.php:98

--- Loader.php
+++ Loader.php
@@ -32,8 +32,8 @@ final class Loader
                 trigger_error(sprintf("You should not use '%s' as class name prefix", self::YAF_LOADER_RESERVERD), E_WARNING);
             }
 
-            // TODO
             if (self::isCategory($class_name, self::YAF_LOADER_MODEL, self::YAF_LOADER_LEN_MODEL)) {
+                /* this is a model class */
                 $directory = sprintf("%s%c%s", $app_directory, DIRECTORY_SEPARATOR, self::YAF_MODEL_DIRECTORY_NAME);
                 $file_name_len = strlen($class_name) - $separator_len - self::YAF_LOADER_LEN_MODEL;
 
@@ -46,10 +46,79 @@ final class Loader
                 break;
             }
 
-            // TODO 从这里开始哟 2018-11-10 20:14
+            if (self::isCategory($class_name, self::YAF_LOADER_PLUGIN, self::YAF_LOADER_LEN_PLUGIN)) {
+                /* this is a plugin class */
+                $directory = sprintf("%s%c%s", $app_directory, DIRECTORY_SEPARATOR, self::YAF_PLUGIN_DIRECTORY_NAME);
+                $file_name_len = strlen($class_name) - $separator_len - self::YAF_LOADER_LEN_PLUGIN;
 
+                if (YAF_G('name_suffix')) {
+                    $file_name = substr($class_name, 0, $file_name_len);
+                } else {
+                    $file_name = substr($class_name, self::YAF_LOADER_LEN_PLUGIN + $separator_len);
+                }
+
+                break;
+            }
+
+            if (self::isCategory($class_name, self::YAF_LOADER_CONTROLLER, self::YAF_LOADER_LEN_CONTROLLER)) {
+                /* this is a controller class */
+                $directory = sprintf("%s%c%s", $app_directory, DIRECTORY_SEPARATOR, self::YAF_CONTROLLER_DIRECTORY_NAME);
+                $file_name_len = strlen($class_name) - $separator_len - self::YAF_LOADER_LEN_CONTROLLER;
+
+                if (YAF_G('name_suffix')) {
+                    $file_name = substr($class_name, 0, $file_name_len);
+                } else {
+                    $file_name = substr($class_name, self::YAF_LOADER_LEN_CONTROLLER + $separator_len);
+                }
+
+                break;
+            }
+
+            if (YAF_G('st_compatible')
+                && strncmp($class_name, self::YAF_LOADER_DAO, self::YAF_LOADER_LEN_DAO) == 0
+                && strncmp($class_name, self::YAF_LOADER_SERVICE, self::YAF_LOADER_LEN_SERVICE) == 0
+            ) {
+                $directory = sprintf("%s/%s", $app_directory, self::YAF_MODEL_DIRECTORY_NAME);
+            }
+
+            $file_name_len = strlen($class_name);
+            $file_name     = substr($class_name, 0, strlen($class_name));
 
         } while (0);
+
+        if (!$app_directory && $directory) {
+            trigger_error("Couldn't load a MVC class unless an %s is initialized", Application::class);
+            $ret = 0;
+            goto out;
+        }
+
+        if (!YAF_G('use_spl_autoload')) {
+            /** directory might be NULL since we passed a NULL */
+            // TODO $directory是否为指针
+            // TODO 从这里开始
+            if ($this->internalAutoload($file_name, $file_name_len, $directory)) {
+                $lc_classname = substr($origin_classname, 0, strlen($class_name));
+                if (class_exists($lc_classname, false)) {
+                    goto out;
+                }
+
+                trigger_error(sprintf("Could not find class %s in %s", $class_name, $directory), E_STRICT);
+            } else {
+                trigger_error(sprintf("Failed opening script %s", $directory), E_WARNING);
+            }
+            goto out;
+        } else {
+            $lower_case_name = strtolower(substr($origin_classname, 0, strlen($class_name)));
+            if ($this->internalAutoload($file_name, $file_name_len, $directory) && class_exists($lower_case_name, false)) {
+                goto out;
+            }
+
+            $ret = 0;
+            goto out;
+        }
+
+out:
+        return (bool) $ret;
     }
 
     /**
@@ -125,21 +194,26 @@ final class Loader
     /**
      * @internal
      */
-    public const YAF_LIBRARY_DIRECTORY_NAME    = "library";
-    public const YAF_CONTROLLER_DIRECTORY_NAME = "controllers";
-    public const YAF_PLUGIN_DIRECTORY_NAME     = "plugins";
-    public const YAF_MODULE_DIRECTORY_NAME     = "modules";
-    public const YAF_VIEW_DIRECTORY_NAME       = "views";
-    public const YAF_MODEL_DIRECTORY_NAME      = "models";
+    public const YAF_LIBRARY_DIRECTORY_NAME     = "library";
+    public const YAF_CONTROLLER_DIRECTORY_NAME  = "controllers";
+    public const YAF_PLUGIN_DIRECTORY_NAME      = "plugins";
+    public const YAF_MODULE_DIRECTORY_NAME      = "modules";
+    public const YAF_VIEW_DIRECTORY_NAME        = "views";
+    public const YAF_MODEL_DIRECTORY_NAME       = "models";
 
-    private const YAF_LOADER_RESERVERD	       = "Yaf_";
-    private const YAF_LOADER_LEN_RESERVERD     = 3;
-    private const YAF_LOADER_CONTROLLER		   = "Controller";
-    private const YAF_LOADER_LEN_CONTROLLER	   = 10;
-    private const YAF_LOADER_MODEL			   = "Model";
-    private const YAF_LOADER_LEN_MODEL		   = 5;
-    private const YAF_LOADER_PLUGIN			   = "Plugin";
-    private const YAF_LOADER_LEN_PLUGIN		   = 6;
+    private const YAF_LOADER_RESERVERD          = "Yaf_";
+    private const YAF_LOADER_LEN_RESERVERD      = 3;
+    private const YAF_LOADER_CONTROLLER         = "Controller";
+    private const YAF_LOADER_LEN_CONTROLLER     = 10;
+    private const YAF_LOADER_MODEL              = "Model";
+    private const YAF_LOADER_LEN_MODEL          = 5;
+    private const YAF_LOADER_PLUGIN             = "Plugin";
+    private const YAF_LOADER_LEN_PLUGIN         = 6;
+
+    private const YAF_LOADER_DAO                = "Dao_";
+    private const YAF_LOADER_LEN_DAO            = 4;
+    private const YAF_LOADER_SERVICE            = "Service_";
+    private const YAF_LOADER_LEN_SERVICE        = 8;
 
     // ================================================== 内部方法 ==================================================
 
