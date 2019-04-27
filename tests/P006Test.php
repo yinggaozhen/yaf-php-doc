f350aec5cc9ed463e249ee5496fda63817dd180c
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-04-27 17:03:18 +0800

    add 006

---
+++ tests/P006Test.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tests;
+use Yaf\Request\Http;
+use Yaf\Route\Route_Static;
+
+/**
+ * @run ./vendor/bin/phpunit --bootstrap P006Test.php
+ */
+class P006Test extends Base
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function test()
+    {
+        $request_uri = '/prefix/controller/action/name/laruence/age/28';
+        $base_uri	 = '/prefix/';
+
+        $request = new Http($request_uri, $base_uri);
+        $route = new Route_Static();
+
+        $this->assertTrue($route->route($request));
+        $this->assertNull($request->module);
+        $this->assertSame('controller', $request->controller);
+        $this->assertSame('action', $request->action);
+        $this->assertSame('Cli', $request->method);
+        $this->assertSame([
+            'name' => 'laruence',
+            'age' => 28
+        ], $request->getParams());
+        $this->assertSame('/prefix/', $request->getBaseUri());
+        $this->assertSame('/prefix/controller/action/name/laruence/age/28', $request->getRequestUri());
+    }
+
+    public function tearDown()
+    {
+    }
+}
