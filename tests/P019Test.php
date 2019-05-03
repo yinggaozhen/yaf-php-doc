c78dc60addd8f6486dcc97ee64054cddeb3af70f
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-05-03 20:55:40 +0800

    add P019Test case -> Yaf_route add route

---
+++ tests/P019Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace tests;
+
+use Yaf\Request\Http;
+use Yaf\Router;
+
+/**
+ * @run ./vendor/bin/phpunit --bootstrap ./tests/bootstrap.php ./tests/P019Test.php
+ */
+class P019Test extends Base
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function test()
+    {
+        $request = new Http('/subdir/ap/1.2/name/value', '/subdir');
+        $router = new Router();
+
+        $router->addConfig(
+            [
+                [
+                    'type' => 'regex',
+                    'match' => '#^/ap/([^/]*)/*#i',
+                    'route' => [
+                        [
+                            'action' => 'ap',
+                        ],
+                    ],
+                    'map' => [
+                        1 => 'version',
+                    ]
+                ]
+            ]
+        )->route($request);
+
+        $this->assertSame(0, $router->getCurrentRoute());
+        $this->assertSame('1.2', $request->getParam('version'));
+    }
+}
