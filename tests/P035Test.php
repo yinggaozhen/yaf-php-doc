f248778e9682a7e0604edf33a88d738f5bfa6431
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-05-23 16:45:13 +0800

    add : add new test case P035 -> Check for Yaf_View_Simple with short_tag_open

---
+++ tests/P035Test.php
@@ -0,0 +1,29 @@
+<?php
+
+use tests\Base;
+
+/**
+ * Check for Yaf_View_Simple with short_tag_open
+ *
+ * @run ./vendor/bin/phpunit --bootstrap ./tests/bootstrap.php ./tests/P035Test.php
+ */
+class P035Test extends Base
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test()
+    {
+        // skip
+    }
+
+    public function tearDown()
+    {
+        parent::setUp();
+    }
+}
