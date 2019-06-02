e1f5a34086be647d2e77005a13064a0f1691d71d
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-06-02 22:25:52 +0800

    Restore apl_autoload_register

--- psr4/Loader.php
+++ psr4/Loader.php
@@ -479,8 +479,7 @@ out:
         $autoload = [$loader, $method];
 
         try {
-            // TODO
-            // spl_autoload_register($autoload);
+            spl_autoload_register($autoload);
         } catch (\Exception $e) {
             yaf_trigger_error(E_WARNING, 'Unable to register autoload function autoload');
             return 0;
